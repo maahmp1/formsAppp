0197e9602caafbfa5ec8165976b4fa6cbffa1cf0
Marcela Martins <marcelamartinspena@gmail.com>
2022-11-15 13:54:35 -0300

    changed the colors of login page and added an alert for "forgot password"

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) { }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Email sent',
+      message: 'An email was sent to the email you typed',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
 
 }
